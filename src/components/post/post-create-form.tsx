a205d16ac6cad3ebe5bcf98a77cf45519598eea7
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-07-19 17:18:32 +0100

    add form validations

--- src/components/post/post-create-form.tsx
+++ src/components/post/post-create-form.tsx
@@ -5,20 +5,40 @@ import { useFormState } from "react-dom"
 import FormButton from "@/components/common/form-button";
 
 export default function PostCreateForm() {
+
+    const [formState, action] = useFormState(actions.createPost, {
+        errors: {}
+    })
+
     return (
         <Popover placement="left">
             <PopoverTrigger>
                 <Button color="primary" >New</Button>
             </PopoverTrigger>
             <PopoverContent>
-                <form action={actions.createPost}>
+                <form action={action}>
                     <div className="flex flex-col gap-4 p-4 w-80" >
                         <h3 className="text-lg" >Create a post</h3>
 
-                        <Input name="title" label="Title" labelPlacement="outside" placeholder="title"/>
+                        <Input 
+                            name="title" 
+                            label="Title" 
+                            labelPlacement="outside" 
+                            placeholder="Title"
+                            isInvalid={!!formState.errors.title}
+                            errorMessage={formState.errors.title?.join(', ')}
+                            />
 
-                        <Input name="content" label="Content" labelPlacement="outside" placeholder="content"/>
+                        <Textarea 
+                            name="content" 
+                            label="Content" 
+                            labelPlacement="outside" 
+                            placeholder="Content"
+                            isInvalid={!!formState.errors.content}
+                            errorMessage={formState.errors.content?.join(', ')}
+                            />
 
+                        {formState.errors._form ? <div className="bg-red-100 p-2 border rounded border-red-400 text-red-600 text-xs">{formState.errors._form.join(', ')}</div> : <></> }
                         <FormButton>Create</FormButton>
                     </div>
                 </form>
