0ac187a63993d8194e0eb11780009c427588525f
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-07-21 20:09:53 +0100

    add comment query and nested comments

--- src/app/topics/[slug]/posts/[id]/page.tsx
+++ src/app/topics/[slug]/posts/[id]/page.tsx
@@ -1,3 +1,28 @@
-export default function ViewPostPage() {
-    return <div>View Post</div>
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import paths from "@/paths";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    id: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, id } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.viewTopic(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={id}/>
+      <CommentCreateForm postId={id} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(id)} />
+    </div>
+  );
 }
