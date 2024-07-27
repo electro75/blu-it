e971405d0812c326a8a0aa6ba8fe188fd19279b8
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-07-27 16:26:54 +0100

    fix missing function errro

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import PostCreateForm from "@/components/post/post-create-form";
 import PostList from "@/components/posts/post-list";
 import { fetchPostsByTopicSlug } from "@/db/queries/post";
+import Link from "next/link";
 interface ViewTopicPageProps {
     params: {
         slug: string
@@ -12,16 +13,20 @@ export default function ViewTopicPage({params}: ViewTopicPageProps) {
 
 
     return (
-        <div className="grid grid-cols-4 gap-4 p-4">
-            <div className="col-span-3">
-                <h1 className="text-2-xl font-bold mb-2" >{slug}</h1>
-                <PostList fetchData={() => fetchPostsByTopicSlug(slug)}/>
-            </div>
+        <>
+            <Link className="underline decoration-solid" href='/'>
+                    {"< "}Back to Home
+            </Link>
+            <div className="grid grid-cols-4 gap-4 p-4">
+                <div className="col-span-3">
+                    <h1 className="text-2-xl font-bold mb-2 capitalize" >{slug}</h1>
+                    <PostList fetchData={() => fetchPostsByTopicSlug(slug)}/>
+                </div>
 
-            <div>
-                <PostCreateForm slug={slug}/>
+                <div>
+                    <PostCreateForm slug={slug}/>
+                </div>
             </div>
-        </div>
-        
+        </>
     )
 }
